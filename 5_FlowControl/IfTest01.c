cb98e003e60dd91dea7b1ed26ef2956aba9cab28
Tom <wuyangzerg@gmail.com>
2023-07-20 11:31:11 +0800

    update if test

--- 5_FlowControl/IfTest01.c
+++ 5_FlowControl/IfTest01.c
@@ -17,19 +17,19 @@ int main(void){
 		printf("0==>\n");
 	}
 
-	//C语言中只有0代表是false,其它的包括负数都是true
+	//一，C语言中只有0代表是false,其它的包括负数都是true
 	if(-1){
 		printf("-1==>\n");	//这里执行
 	}
 
-	//char类型也是等于true，实际底层都是数
+	//二，char类型也是等于true，实际底层都是数
 	char b = 'a';
 	if(b){
 		printf("b==>\n");
 		printf("b = %d\n",b);
 	}
 
-	//if..else if... else和Java中一样
+	//三，if..else if... else和Java中一样
 	if(2 > 5){
 		printf("2>5?\n");
 	}else if(2 < 5){
@@ -37,6 +37,23 @@ int main(void){
 	}else{
 		printf("else\n");
 	}
+
+	//四，if后不写大括号默认范围是一条语句。
+	if(2 > 3)
+		printf("test default!"); //这一句不执行
+	printf("not in the domain of if\n"); //这一句执行
+	
+	//五，表达式写法, 60 < j < 100,不正确
+	//   无论j去什么值，结果总为真，因为先执行60<j，结果为0或1,它们恒小于100
+	int j = 200;
+	if(60 < j < 100) 
+		printf("result = %d\n",60<j<100);  //这里一直会执行
+	// 正确写法
+	if(j > 60 && j < 100)
+		printf("won't execute"); //不执行
+
+
+	
 
 	return 0;
 }
