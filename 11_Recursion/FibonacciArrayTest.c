37a634b0ad2a6b2bf51a2be141240107af426293
plms <askdell@sina.com>
2023-09-06 08:55:24 +0800

    Fibonacci array implemented by using for loop

--- 11_Recursion/FibonacciArrayTest.c
+++ 11_Recursion/FibonacciArrayTest.c
@@ -3,6 +3,7 @@
 
 
 /*
+ * Recursion
  * Fibonacci array. Calculate the Nth number in the array.
  * */
 int fibonacci(int n){
@@ -16,11 +17,36 @@ int fibonacci(int n){
 	return k;
 }
 
+//Implementation of Fibonacci array with for loop
+int fibonacciFor(int n){
+	if(n < 0)
+		return 0;
+	//number of index 0 is defined as the first one.
+	if(n <= 1)
+		return n;
+	
+	int fib = 1;
+	int prev = 1;
+	for(int i = 2; i <=n; i++){
+		int temp = fib; 
+		fib += prev;
+		prev = temp;
+	}
+
+	return fib;
+
+}
+
 int main(void){
 
+	//recursion
 	int result = fibonacci(6);
-
 	printf("the Nth number in Fibonacci array is %d\n",result);
+
+	//for loop
+	int resultFor = fibonacciFor(6);
+	printf("for loop,the Nth number in Fibonacci array is %d\n",resultFor);
+
 
 	return 0;
 }
