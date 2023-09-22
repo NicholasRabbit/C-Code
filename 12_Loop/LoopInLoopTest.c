237f4a5ad4fb297d8d4885087a58a8254b960f72
Tom <wuyangzerg@gmail.com>
2023-09-22 15:52:21 +0800

    print rhombus by user for loop

--- 12_Loop/LoopInLoopTest.c
+++ 12_Loop/LoopInLoopTest.c
@@ -20,10 +20,71 @@ int loop(int n){
 
 }
 
+//to print a rhombus
+int rhombus(int rows,char mark){
+	//check if the num is odd.
+	if(rows % 2 == 0){
+		printf("the num must be odd.\n");
+		return 0;
+	}
+	
+	int num = (rows + 1) / 2;
+
+	//first part
+	int i;
+	for(i = 1; i <= num; i++){
+		//print blanks
+		int j;
+		for(j = 1; j <= num - i; j++)
+			printf(" ");
+		//print marks
+		int k = 2 * i - 1;	//the quantity of asterisk
+		int m;
+		for(m = 1; m <= k; m++)
+			printf("%c",mark);
+
+		printf("\n");
+
+
+	}
+
+	//second half
+	int num2 = num - 1;  //从中间向下的所有行数，比总行数一半少一行。
+	
+	int n;
+	for(n = 1; n <= num2; n ++){
+		//print blanks
+		int j;
+		for(j = 1; j <= n; j++)
+			printf(" ");
+		//pirnt marks
+		int k = 2 * (num2 - n + 1) - 1; //the quantity of asterisk
+		int m;
+		for(m = 1; m <= k; m++){
+			printf("%c",mark);
+		
+		}
+
+		//next line
+		printf("\n");
+		
+	
+	}
+		
+
+	return 0;
+}
 
 int main(void){
 
 	loop(9);
+
+	printf("rows:");
+
+	int rows;
+	scanf("%d",&rows);
+
+	rhombus(rows,'*');
 
 	return 0;
 }
