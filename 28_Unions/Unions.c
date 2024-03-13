8b5b55f99d23e41d913d04c3a8a66142278850a6
Tom <wuyangzerg@gmail.com>
2024-03-13 17:36:52 +0800

    union demo

---
+++ 28_Unions/Unions.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+
+/*
+ * union
+ * The syntax is based on structures.
+ * */
+int main(void){
+	
+	union u_tag {
+		char c;
+		short s;
+		int i;
+
+	} demo_union;
+	
+	demo_union.c = 'A';
+
+	printf("union = %c\n", demo_union.c);
+
+
+	return 0;
+}
+
+
