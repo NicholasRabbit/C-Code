d45f8b2502260959a0298af1ae66c5fbac3e431a
Tom <wuyangzerg@gmail.com>
2024-05-07 15:20:25 +0800

    linked file

---
+++ 31_Linked/linked_main.c
@@ -0,0 +1,36 @@
+# include<stdio.h>
+
+/*
+ * Compile two files together.
+ * gcc commands:
+ *
+ * gcc linked_main.c stack.c -o test_linked.out
+ *
+ * OR 
+ *
+ * gcc -c linked_main.c
+ * gcc -c stack.c
+ * gcc linked_main.o stack.o -o test_linked.out
+ *
+ * */
+
+int a, b = 1;
+
+int main(void)
+{
+	push('a');
+	push('b');
+	push('c');
+
+	while(!is_empty()){
+
+		putchar(pop());
+
+	}
+
+	putchar('\n');
+
+	return 0;
+
+}
+
---
+++ 31_Linked/stack.c
@@ -0,0 +1,19 @@
+
+
+char stack[512];
+int top = -1;
+
+void push(char c)
+{
+	stack[++top] = c;
+}
+
+char pop()
+{
+	return stack[top--];
+}
+
+int is_empty()
+{
+	return top == -1;
+}
