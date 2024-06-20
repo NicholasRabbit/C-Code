964b3db8ab86547b40bb049a13295a23c4274b8c
Tom <wuyangzerg@gmail.com>
2024-06-20 14:32:44 +0800

    editting code about unsigend

--- 29_Unsigned_and_Signed/unsigned_and_signed.c
+++ 29_Unsigned_and_Signed/unsigned_and_signed.c
@@ -5,12 +5,13 @@ int main(void)
 {
 	/*
 	 * 1. An unsigned number is always bigger than 0.
-	 * The for loop below is endless.
+	 * The for loop below is endless no matter how many times it minus one.
 	 * */
+	/*
 	unsigned int i;
-	for(i = 10; i < 0; i--){
+	for(i = 10; i > 0; i--){
 		printf("%u\n", i);
-	}
+	} */
 	
 	// 2. We can add "U/u" as suffix to declare unsigned constants.
 	unsigned num = 128U;
@@ -22,7 +23,8 @@ int main(void)
 	 *    the other is unsigned, C implicitly cast the signed argument to
 	 *    unsigned.
 	 * */
-	int result = -1 < 0U;
+	// -1 is 0xffffffff = 4294967295U. So the result is 0, namely false.
+	int result = -1 < 0U;  
 	printf("-1 < 0U ==> %d\n", result);
 	
 
@@ -31,12 +33,8 @@ int main(void)
 	 * */
 	int s = -1;
 	unsigned int u = (unsigned)s;
-	printf("unsigned value of -1==> %u\n", u);
-
-	unsigned char ux = 128;
-	char tx = (char)ux;
-	printf("")
-
+	printf("unsigned value of -1(int)==> %u\n", u);
 
 	return 0;
+
 }
