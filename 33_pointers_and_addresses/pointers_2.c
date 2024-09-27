10c2fdf671123e802b8233b01fe503dec2c5fd29
Tom <wuyangzerg@gmail.com>
2024-09-27 16:58:36 +0800

    pointers-2

--- 33_pointers_and_addresses/pointers_2.c
+++ 33_pointers_and_addresses/pointers_2.c
@@ -5,15 +5,55 @@ int main (void)
 {
 
 	/*
-	 * 1. A pointer to 'void' is used to hold any type of pointer but can not 
-	 *    be used to dereference itself.
+	 * 1.Every pointer point to a specifice data type. 
+	 * Whereas, there is an exception: 
+	 * A pointer to 'void' is used to hold any type of pointer 
+	 * but can not be used to dereference itself.
 	 * */
 	void *p;
 
 	/*
-	 * 2. 
+	 * 2. Pointers can be pointers. For example, in the function "atof(char *)" the 
+	 * argument is a pointer to char.
 	 * */
 	double *dp, atof(char *);
+
+	/*
+	 * 3. If 'ip' is a pointer which points to 'x' it can occur/appear any context 
+	 * where x could.
+	 * */
+	int x = 10;
+	int y = 3;
+	int *ip;
+	ip = &x;
+	
+	*ip = *ip + 2; // x = x + 2
+	
+	y = *ip + 5;
+	
+	*ip += 1;
+
+	++(*ip);
+
+	/*
+	 * 4. N.B. "*ip" should be put in parentheses because unary operators like 
+	 * "++, *" associate right to left. If not, "ip" will increment 1.
+	 * */ 
+	(*ip)++; 
+
+	printf("*ip is %d\n", *ip);
+	printf("y is %d\n", y);
+
+	/*
+	 * 5. Since pointers are variables, they can be used without deferencing. 
+	 * For example, we can directly use the pointer "ip" to assign its value
+	 * to another pointer.
+	 * */
+	int *iq;
+	iq = ip;
+	printf("*iq is %d\n", *iq);
+
+
 
 
 	return 0;
