fd866f52ebb541575673c77f9a48ce32e4ab30e9
Tom <wuyangzerg@gmail.com>
2024-10-12 15:13:17 +0800

    modifying some comments in the code

--- 12_Loop/ContinueInForAndWhile.c
+++ 12_Loop/ContinueInForAndWhile.c
@@ -1,18 +1,17 @@
 # include<stdio.h>
 
 /*
- * The for loop is as same as the  while loop in most circunmstances,but there is a dramatic difference
- * when the "continue" is used in them.
- *
+ * The for loop is as same as the  while loop in most circunmstances,
+ * but there is a significant difference when the "continue" is used in them.
  * */
 
 
 int forLoop(){
 
 	int i;
-	for(i = 0; i <= 10; i++){  //当continue执行后，在大括号内其下面的语句不会执行，但是本行的i++会执行。
+	for(i = 0; i <= 10; i++){  
 		if(i % 2 == 0)
-			continue;
+			continue;   // the code below won't be executed but "i++" in the parentheses will.
 		printf("for=%d\n",i);
 	}
 
@@ -26,7 +25,7 @@ int whileLoop(){
 	while(i <= 10){
 		printf("while=%d\n",i);
 		if(i % 2 == 0)
-			continue;		//while循环中，continue执行后，下面的都不执行，包括i++，因此这里会出现死循环。
+			continue;	// the following statement won't be executed including "i++";
 		printf("while=%d\n",i);
 		i ++;
 	}
