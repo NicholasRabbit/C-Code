db1abb4703ee3b4e755f16bee1ed83c7730e253f
Tom <wuyangzerg@gmail.com>
2024-10-12 11:39:03 +0800

    review %d,f...

--- 2_ASCIIandPrintfTest/printf_test_001.c
+++ 2_ASCIIandPrintfTest/printf_test_001.c
@@ -5,26 +5,11 @@
  * 1,输出控制符范例。
  * */
 int main(void){
-	int i = 10;
+	
 	//一，输出控制符
 	
 	/* 
-	 * 1,%d表示把i以10进制的形式输出;
-	 *   %u: 输出无符号的int类型数,对应的是有符号的%d
 	 *
-	 * 2,%x,%X表示以16进制输出，这俩意思一样,一般都用大写，好区分;
-	 *   %.2x : That indicates that an integer should be printed in hexadecimal 
-	 *          with at leat 2 digits. 
-	 *
-	 * 3,%f表示单浮点精度float;
-	 *   %lf表示双浮点精度double
-	 *   但是对于prinf()函数来说没有区别，printf()会自动把float向上转型为double输出。
-	 *   对于scanf()有区别，具体见本目录下: DoubleFloatAndPrintfScanf.c内解释。
-	 *	 %3.0f: It means that a floating number is to be printed at least 
-	 *	        three characters wide without no decimal point or fractinal digits.
-	 *	        For example: 3.22 -> 3
-	 *	 %6.2f: It means that a floating number is to be printed as least
-	 *	        six characters wide with 1 digit after the decimal point.
 	 *
 	 *
 	 * 4,%ld: signed long (long decimal缩写，默认是有符号的)
@@ -33,27 +18,62 @@ int main(void){
 	 * 5,%p : 这个输出控制符表示输出指针，即变量的地址，注意打印时在变量前加“&”表示取地址。
 	 *   See PrintfTes002.c
 	 *
-	 * 6,%c : to print a character on the ASCII talbe.
+	 * 6,%c : to pirnt the representation of character of arguments.
 	 * */
-	printf("%d\n",i);   //%d默认打印有符号的int类型(signed  integer)
-	printf("%X\n",i);	//10用16进制表示就是a,即9后面就是a。
 
+	/*
+	 * 0, How to print '%' itself?
+	 *	  Using "%%".
+	 * */
+	printf("%%\n");
+
+
+	/*
+	 * 1,%d: 'd' is short for "decimal". It means that to print the value 
+	 *       in a decimal integer.
+	 *   %u: to print unsigend decimal integer.
+	 *   
+	 *   %6d: print a decimal integer with at least 6 characters.
+	 * */
+	int i;
+	i = -1;
+	printf("%%d: %d\n",i);   
+	printf("u: %u\n", i);
+	printf("6d: %6d\n", i);
+
+	/*
+	 * 2,%x or %X: To print the hexadecimal value of a argument.
+	 *   %.2x : That indicates that an integer should be printed in hexadecimal 
+	 *          with at leat 2 digits. 
+	 * */
 	int j = 15;
-	printf("j's hexadecimal is %X\n",j);
+	printf("j's hexadecimal value is %X\n", j);
 
-	float age = 12.9;
-	printf("age = %f\n",age); //这行和下行都是输出6位精度，待研究。printf()函数把单浮点的自动向上转型了。
+	/*
+	 * 3,%f: print as floating point.
+	 *   %lf: print as double floating point.
+	 *
+	 *	 %3.0f: It means that a floating number is to be printed at least 
+	 *	        three characters wide without no decimal point or fractinal digits.
+	 *	        For example: 3.22 -> 3
+	 *	 %6.2f: It means that a floating number is to be printed as least
+	 *	        six characters wide with 1 digit after the decimal point.
+	 *
+	 *   N.B. The function "printf(...)" casts 'float' to 'double' by default,
+	 *   but "scanf(...)" doesn't. See "DoubleFloatAndPrintfScanf.c".
+	 * */
+	float f0, f1, f2;
+	f0 = 12.9;
+	printf("f0 = %f\n", f0); 
 
-	float f1 = 20.0 / 6.0;
-	float f2 = 3.020304; 
+	f1 = 20.0 / 6.0;
+	f2 = 3.020304; 
 	printf("f1 = %3.0f\n", f1);
 	printf("f1 = %6.2f\n", f2);
 
-	double money = 82.35;
-	printf("money = %lf\n",money);
+	double d1 = 82.35;
+	printf("d1 = %lf\n", d1);
 
-	long k = 0xfffffff; //32个1,是int的最大数，在int范围里表示-1
-	printf("signed long = %ld\n",k);
 
 	//二，long输出控制符。
 	//这里写不写unsigned关键字都可以，输出的时候使用"%lu"就可以打印无符号的数。
@@ -62,6 +82,9 @@ int main(void){
 	unsigned long m = 0xffffffffffffffff;
 	printf("signed long(m) = %ld\n", m);   //signed有符号；表示-1
 	printf("unsigned long(m) = %lu\n", m); //unsigned无符号：表示：2^32次方
+
+	long k = 0xfffffff; //32个1,是int的最大数，在int范围里表示-1
+	printf("signed long = %ld\n",k);
 
 	//三，%u
 	int n = -1;
