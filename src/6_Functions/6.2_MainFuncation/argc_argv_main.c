719a9da65ce0df7787bf1af195f12a6496d6c738
plms <wuyangzerg@gmail.com>
2025-06-04 17:10:47 +0800

    argc, argv[] in main(...)

---
+++ src/6_Functions/6.2_MainFuncation/argc_argv_main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ * "argc" is short for "argumetns count" which indicates the nubmer 
+ * of the input arguments.
+ * "argv[]" stores the arguments as char array.
+ *
+ * Run:
+ * ./argc_argv_main.out a b c
+ * The output is
+ * 0 ./argc_argv_main
+ * 1 a
+ * 2 b
+ * 3 c
+ *
+ * */
+
+int main(int argc, char *argv[])
+{
+	printf("The number of arguments is: %d\n", argc);
+
+	int i;
+	for (i = 0; i < argc; i++) {
+		printf("Argument: %d %s\n", i, argv[i]);
+	}
+	return 0;
+}
